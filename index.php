3982e2e33bb9690d621b8419adcd15f562bdf3e8
Kongyot Rojphutthikul <kongtoonarmy@hotmail.com>
2016-05-12 18:48:02 +0700

    Start to prepare a class that base on the template method pattern
    - Created TurkeySub class
    - Call a method by chaining

--- index.php
+++ index.php
@@ -2,20 +2,4 @@
 
 require 'vendor/autoload.php';
 
-use Acme\Book;
-use Acme\BookInterface;
-use Acme\Kindle;
-use Acme\Nook;
-use Acme\eReaderAdapter;
-
-class Person {
-	public function read(BookInterface $book)
-	{
-		$book->open();
-		$book->turnPage();
-	}
-}
-
-(new Person)->read(new eReaderAdapter(new Kindle));
-echo '<br>';
-(new Person)->read(new eReaderAdapter(new Nook));
+(new App\TurkeySub)->make();
